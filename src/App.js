0604464064cf786386aa2088d977c038647864c5
Gaston <gastonsalaya@gmail.com>
2023-12-14 19:46:28 -0300

    Clon de Netflix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const API_KEY = 'bebdacd451ebbfdcd7956c1153ffbcf2'; // API de TMDB
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchMovies() {
+      try {
+        const response = await axios.get(
+          `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&page=1`
+        );
+        setMovies(response.data.results);
+      } catch (error) {
+        console.error('Error fetching movies:', error);
+      }
+    }
+
+    fetchMovies();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Clon de Netflix</h1>
+      <div className="movie-container">
+        {movies.map((movie) => (
+          <div key={movie.id} className="movie">
+            <img
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              alt={movie.title}
+            />
+            <h3>{movie.title}</h3>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
